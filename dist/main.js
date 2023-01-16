ebb6f96f38fcd4b8d57e1a26033186fbe9fe3400
Vika Oneil <oneilcode111@gmail.com>
2023-01-17 00:32:28 +1000

    Выполнила модуль плавный скрол меню

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";console.log("test")})();
+(()=>{"use strict";document.querySelector(".top-menu").querySelectorAll("ul > li > a").forEach((e=>{e.addEventListener("click",(e=>{e.preventDefault();const t=e.target.getAttribute("href").substr(1);document.getElementById(t).scrollIntoView({behavior:"smooth",block:"start"})}))}))})();
