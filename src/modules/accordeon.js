8e6ebaa546ef1ecf1f6ddf94f2fddc3ae9ca9bed
Vika Oneil <oneilcode111@gmail.com>
2023-01-19 23:33:27 +1000

    Аккардион

--- src/modules/accordeon.js
+++ src/modules/accordeon.js
@@ -1,5 +1,7 @@
 const accordeon = () => {
    const elements = document.querySelectorAll('.element');
+   const contents = document.querySelectorAll('.element-content');
+   const titles = document.querySelectorAll('.title');
 
    elements.forEach(el => {
       el.addEventListener('click', (e) => {
@@ -10,12 +12,12 @@ const accordeon = () => {
          self.classList.toggle('active');
 
          if (self.classList.contains('active')) {
-
             content.style.display = 'block';
          } else {
             content.style.display = 'none';
             el.classList.remove('active');
          }
+         content.classList.add('active');
       });
    });
 };
